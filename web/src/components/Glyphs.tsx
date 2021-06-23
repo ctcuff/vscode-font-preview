a4173e02a86bd87d1640d954f1235d4f3c9f35e7
ctcuff <dev.ctcuff@gmail.com>
2021-06-23 19:44:12 -0400

    Glyphs can now be copied to the clipboard

--- web/src/components/Glyphs.tsx
+++ web/src/components/Glyphs.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable */
 import '../scss/glyphs.scss'
-import React, { useContext } from 'react'
+import 'react-toastify/dist/ReactToastify.css'
+import React, { useContext, useEffect, useState } from 'react'
+import { ToastContainer, toast, cssTransition } from 'react-toastify'
 import FontContext from '../contexts/FontContext'
 import FontNameHeader from './FontNameHeader'
 
@@ -10,46 +11,87 @@ type GlyphData = {
   name: string
 }
 
+const entityTextArea = document.createElement('textarea')
+
+const copyGlyphToClipboard = (glyph: string): void => {
+  // The glyph will be encoded (for example F => &#70;) so we need
+  // to put the glyph in a text area in order to copy the decoded version
+  entityTextArea.innerHTML = glyph
+
+  navigator.clipboard
+    .writeText(entityTextArea.value)
+    .then(() => {
+      toast('Glyph copied to clipboard', {
+        position: 'bottom-right',
+        className: 'react-toast',
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnFocusLoss: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        transition: cssTransition({
+          enter: 'react-toast__enter',
+          exit: 'react-toast__exit'
+        })
+      })
+    })
+    // eslint-disable-next-line no-console
+    .catch(err => console.error(err))
+}
+
 const Glyphs = (): JSX.Element => {
   const { font } = useContext(FontContext)
-  const glyphs = font.glyphs
-  const glyphsList: GlyphData[] = []
+  const [glyphs, setGlyphs] = useState<GlyphData[]>([])
 
-  for (let i = 0; i < glyphs.length; i++) {
-    const glyph = glyphs.get(i)
-    const { leftSideBearing, rightSideBearing, yMin, yMax } = glyph.getMetrics()
+  useEffect(() => {
+    const glyphList: GlyphData[] = []
 
-    // Glyphs that have a height of 0 for their bounding box are either
-    // empty or won't be visible so we'll skip them
-    if (leftSideBearing === 0 && rightSideBearing === 0 && yMin === 0 && yMax === 0) {
-      continue
+    for (let i = 0; i < font.glyphs.length; i++) {
+      const glyph = font.glyphs.get(i)
+      const { leftSideBearing, rightSideBearing, yMin, yMax } = glyph.getMetrics()
+
+      // Glyphs that have a height of 0 for their bounding box are either
+      // empty or won't be visible so we'll skip them
+      if (leftSideBearing === 0 && rightSideBearing === 0 && yMin === 0 && yMax === 0) {
+        continue
+      }
+
+      // Skip rendering null glyphs and the space character
+      // since these characters can break the grid layout
+      if (glyph.unicode !== undefined && glyph.unicode !== 32) {
+        glyphList.push({
+          htmlEntity: `&#${glyph.unicode};`,
+          glyphIndex: i,
+          name: glyph.name
+        })
+      }
+
+      setGlyphs(glyphList)
     }
-
-    // Skip rendering null glyphs and the space character
-    // since these characters can break the grid layout
-    if (glyph.unicode !== undefined && glyph.unicode !== 32) {
-      glyphsList.push({
-        htmlEntity: `&#${glyph.unicode};`,
-        glyphIndex: i,
-        name: glyph.name
-      })
-    }
-  }
+  }, [])
 
   return (
     <div className="glyphs">
+      <ToastContainer limit={1} />
       <FontNameHeader />
-      {glyphsList.map(({ htmlEntity, glyphIndex, name }, i) => (
+      {glyphs.map(({ htmlEntity, glyphIndex, name }, i) => (
         <div
           className="glyph"
           title={name}
           key={glyphIndex}
           data-index={i}
-          onClick={() => {
-            console.log({ index: i }, glyphs.get(glyphIndex))
-          }}
+          onClick={() => copyGlyphToClipboard(htmlEntity)}
         >
-          <span dangerouslySetInnerHTML={{ __html: htmlEntity }} />
+          <span
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              // In order for the editor to render the decoded glyph, we need to
+              // render it as an unsafe string rather than a string inside a component
+              __html: htmlEntity
+            }}
+          />
           <div className="glyph-detail">{name}</div>
         </div>
       ))}
